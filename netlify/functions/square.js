41dda252ece127625794cf378dd9662c0e7cae3a
ararymcgowan-design <ararymcgowan@gmail.com>
2026-04-25 11:03:22 -0400

    Update square.js

--- netlify/functions/square.js
+++ netlify/functions/square.js
@@ -11,7 +11,26 @@ exports.handler = async function(event) {
   }
 
   try {
-    const { endpoint, method, body } = JSON.parse(event.body);
+    let { endpoint, method, body } = JSON.parse(event.body);
+
+    function cleanPhone(phone) {
+      if (!phone) return undefined;
+      const digits = phone.replace(/\D/g, '');
+      if (digits.length === 10) return '+1' + digits;
+      if (digits.length === 11 && digits[0] === '1') return '+' + digits;
+      return undefined;
+    }
+
+    if (endpoint === 'customers' && body) {
+      if (body.phone_number) {
+        body.phone_number = cleanPhone(body.phone_number);
+      }
+      Object.keys(body).forEach(k => body[k] === undefined && delete body[k]);
+      if (body.address) {
+        Object.keys(body.address).forEach(k => body.address[k] === undefined && delete body.address[k]);
+      }
+    }
+
     const response = await fetch('https://connect.squareup.com/v2/' + endpoint, {
       method: method || 'POST',
       headers: {
@@ -21,6 +40,7 @@ exports.handler = async function(event) {
       },
       body: body ? JSON.stringify(body) : undefined
     });
+
     const data = await response.json();
     return {
       statusCode: response.status,
